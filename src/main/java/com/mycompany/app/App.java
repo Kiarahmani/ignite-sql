b2d9e12b1464d96178572470039b6e0a59872df6
Ubuntu <ubuntu@ip-172-31-24-136.us-east-2.compute.internal>
2018-08-03 17:23:24 +0000

    commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -143,7 +143,7 @@ public class App
     public static final int _ROUNDS = 1;
     public static final boolean _CHOPPED = true;
     public static final boolean _MASTER = true;
-    public static final int _CLIENT_NUMBER = 680;
+    public static final int _CLIENT_NUMBER = 512;
     public static final int _STUDENT_COUNT = 8;
     public static final int _INSTRUCTOR_COUNT = 10;
     public static final int _COLLEGE_COUNT = 5;
