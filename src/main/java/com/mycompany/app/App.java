5f32062dc22f693517a8413260b1d85b0319d4ea
Ubuntu <ubuntu@ip-172-31-24-136.us-east-2.compute.internal>
2018-08-03 21:25:46 +0000

    read college at the end

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -143,7 +143,7 @@ public class App
     public static final int _ROUNDS = 1;
     public static final boolean _CHOPPED = true;
     public static final boolean _MASTER = true;
-    public static final int _CLIENT_NUMBER = 500;
+    public static final int _CLIENT_NUMBER = 750;
     public static final int _STUDENT_COUNT = 12;
     public static final int _INSTRUCTOR_COUNT = 10;
     public static final int _COLLEGE_COUNT = 5;
