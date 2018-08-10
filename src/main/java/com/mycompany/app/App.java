eed8904130d59f49fb01be51c1ee731e4671f905
Ubuntu <ubuntu@ip-172-31-24-136.us-east-2.compute.internal>
2018-08-10 20:14:09 +0000

    commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -144,10 +144,11 @@ public class App
     public static final boolean _CHOPPED = false;
     public static final boolean _MASTER = true;
     public static final int _CLIENT_NUMBER = 2;
-    public static final int _STUDENT_COUNT = 12;
-    public static final int _INSTRUCTOR_COUNT = 10;
-    public static final int _COLLEGE_COUNT = 5;
-    public static final int _COURSE_COUNT = 7;
+    public static final int _TEMP = 2;
+    public static final int _STUDENT_COUNT = _TEMP*4;
+    public static final int _INSTRUCTOR_COUNT = _TEMP*2;
+    public static final int _COLLEGE_COUNT = _TEMP;
+    public static final int _COURSE_COUNT = _TEMP*2;
     public static final int _LAT_THRESHOLD = 4000;
     public static final int _TRANSCRIPT_COUNT = _STUDENT_COUNT*_COURSE_COUNT;
     public static final int _REGISTER_COUNT = _STUDENT_COUNT*_COURSE_COUNT;
