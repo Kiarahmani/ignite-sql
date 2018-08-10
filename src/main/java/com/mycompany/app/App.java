7b54fa5e4b698378f85620761b4320e6f88eb70a
Ubuntu <ubuntu@ip-172-31-24-136.us-east-2.compute.internal>
2018-08-10 18:56:19 +0000

    commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -140,10 +140,10 @@ class ConsoleColors {
 //////////////////////////////////////////////////////////////////////////////////////////////
 public class App 
 {
-    public static final int _ROUNDS = 100;
+    public static final int _ROUNDS = 200;
     public static final boolean _CHOPPED = false;
     public static final boolean _MASTER = true;
-    public static final int _CLIENT_NUMBER = 5;
+    public static final int _CLIENT_NUMBER = 2;
     public static final int _STUDENT_COUNT = 12;
     public static final int _INSTRUCTOR_COUNT = 10;
     public static final int _COLLEGE_COUNT = 5;
