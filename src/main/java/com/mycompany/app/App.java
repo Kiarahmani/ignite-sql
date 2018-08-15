e9f3a56283649c8d5d83b1fe4e0eb7a8860c1a49
Kiarahmani <rahmank@purdue.edu>
2018-08-15 19:46:51 -0400

    test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,8 +1,9 @@
 // things are going smoothely!
 
 package com.mycompany.app;
+
 import java.util.*;
-import  java.util.concurrent.atomic.AtomicLongArray;
+import java.util.concurrent.atomic.AtomicLongArray;
 import java.util.concurrent.ThreadLocalRandom;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.lang.*;
@@ -30,54 +31,49 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.net.UnknownHostException;
 import java.util.UUID;
 
-
-public class App 
-{
+public class App {
 	public static final int _CLIENT_NUMBER = 8;
 	public static final int _OBJECTS = 10;
-	public static final int _ROUNDS = 100/_CLIENT_NUMBER;
-	static long[] myArray = new long[_CLIENT_NUMBER*_ROUNDS];
+	public static final int _ROUNDS = 100 / _CLIENT_NUMBER;
+	static long[] myArray = new long[_CLIENT_NUMBER * _ROUNDS];
 	private static AtomicLongArray at = new AtomicLongArray(myArray);
+
 	public static void main(String[] args) {
-		//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-		
-                IgniteConfiguration cfg = new IgniteConfiguration();
+		// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+		IgniteConfiguration cfg = new IgniteConfiguration();
 		cfg.setClientMode(true);
 		// ADDRESS RESOLVER
-		Map<String,String> addrMap = new HashMap<String,String>();
-		addrMap.put("172.31.19.186", "52.14.206.156"); //port forwarding
-		BasicAddressResolver addrRes=null;
+		Map<String, String> addrMap = new HashMap<String, String>();
+		addrMap.put("172.31.19.186", "52.14.206.156"); // port forwarding
+		BasicAddressResolver addrRes = null;
 		try {
 			addrRes = new BasicAddressResolver(addrMap);
-		}catch(UnknownHostException e){System.out.println(e);}
+		} catch (UnknownHostException e) {
+			System.out.println(e);
+		}
 		cfg.setAddressResolver(addrRes);
 		// IP FINDER
 		TcpDiscoverySpi spi = new TcpDiscoverySpi();
 		TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
-		ipFinder.setAddresses(Arrays.asList("54.169.147.152:47500..47509"));//Ohio Client
+		ipFinder.setAddresses(Arrays.asList("54.169.147.152:47500..47509"));// Ohio Client
 		spi.setIpFinder(ipFinder);
 		cfg.setDiscoverySpi(spi);
-		
+
 		// FIRE IGNITE
 		long startTime = System.currentTimeMillis();
 		Ignite ignite = Ignition.start(cfg);
 		IgniteTransactions transactions = ignite.transactions();
-		System.out.println("All Available Caches on server : "+ignite.cacheNames());
-		
-			
-		// CREATE A NEAR CACHE
-		NearCacheConfiguration<Integer, Integer> nearCfg = 
-			    new NearCacheConfiguration<>();
+		System.out.println("All Available Caches on server : " + ignite.cacheNames());
 
-		
-		//INITILIZE A CACHE	
-		CacheConfiguration<Integer,Integer> ccfg = new CacheConfiguration<Integer,Integer>("sync");
+		// INITILIZE A CACHE
+		CacheConfiguration<Integer, Integer> ccfg = new CacheConfiguration<Integer, Integer>("sync");
 		ccfg.setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL);
 		ccfg.setCacheMode(CacheMode.PARTITIONED);
 		ccfg.setBackups(1);
 		ccfg.setWriteSynchronizationMode(CacheWriteSynchronizationMode.PRIMARY_SYNC);
-		//INITILIZE A CACHE	
-		CacheConfiguration<Integer,Integer> sccfg = new CacheConfiguration<Integer,Integer>("stale_sync");
+		// INITILIZE A CACHE
+		CacheConfiguration<Integer, Integer> sccfg = new CacheConfiguration<Integer, Integer>("stale_sync");
 		sccfg.setAtomicityMode(CacheAtomicityMode.ATOMIC);
 		sccfg.setCacheMode(CacheMode.PARTITIONED);
 		sccfg.setWriteSynchronizationMode(CacheWriteSynchronizationMode.PRIMARY_SYNC);
@@ -86,66 +82,51 @@ public class App
 		RendezvousAffinityFunction affFunc = new RendezvousAffinityFunction();
 		affFunc.setExcludeNeighbors(true);
 		affFunc.setPartitions(1);
-		ccfg.setAffinity(affFunc);		
-		//create cache
+		ccfg.setAffinity(affFunc);
+		// create cache
 		IgniteCache<Integer, Integer> cache = ignite.getOrCreateCache(ccfg);
-		IgniteCache<Integer, Integer> stale_cache = ignite.getOrCreateCache(sccfg,nearCfg);
+		IgniteCache<Integer, Integer> stale_cache = ignite.getOrCreateCache(sccfg);
 		Set<Integer> all_keys = new TreeSet<Integer>();
-		for (int i=0; i<_OBJECTS; i++){
+		for (int i = 0; i < _OBJECTS; i++) {
 			System.out.print(".");
-			cache.put(i,0);
-			stale_cache.put(i,1000000);
+			cache.put(i, 0);
+			stale_cache.put(i, 1000000);
 			all_keys.add(i);
 		}
-		
-		
-		
+
 		TransactionIsolation ser = TransactionIsolation.SERIALIZABLE;
 		TransactionIsolation rc = TransactionIsolation.READ_COMMITTED;
 		TransactionConcurrency concurrency = TransactionConcurrency.PESSIMISTIC;
-		//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+		// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 		// TESTS
-			
-	
-	// PRINT STATS
-	
 
+		// PRINT STATS
 
-		//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	    		
+		// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 		// PRINT THE FINAL VALUES
-		int sum=0;
-		try (Transaction tx = transactions.txStart(concurrency,TransactionIsolation.SERIALIZABLE)) {
+		int sum = 0;
+		try (Transaction tx = transactions.txStart(concurrency, TransactionIsolation.SERIALIZABLE)) {
 			System.out.println(">> final values:");
-			int currentVal=0;
-			for (int i=0; i<_OBJECTS; i++){
-				currentVal=cache.get(i);
-				sum+=currentVal;
-				System.out.println("("+i+","+currentVal+")");
+			int currentVal = 0;
+			for (int i = 0; i < _OBJECTS; i++) {
+				currentVal = cache.get(i);
+				sum += currentVal;
+				System.out.println("(" + i + "," + currentVal + ")");
 			}
 		}
-    System.out.print("\n\n\n\n===========================================\n");
-		System.out.println("\nSafety:  "+(_ROUNDS*_CLIENT_NUMBER==sum));
-		//System.out.println(  "Throuput:"+ ((_ROUNDS)*_CLIENT_NUMBER)*1000/estimatedTime_tp+" rounds/s");
-		int sum_time=0;
-		for (int i=0; i<_CLIENT_NUMBER*_ROUNDS; i++ ){
+		System.out.print("\n\n\n\n===========================================\n");
+		System.out.println("\nSafety:  " + (_ROUNDS * _CLIENT_NUMBER == sum));
+		// System.out.println( "Throuput:"+
+		// ((_ROUNDS)*_CLIENT_NUMBER)*1000/estimatedTime_tp+" rounds/s");
+		int sum_time = 0;
+		for (int i = 0; i < _CLIENT_NUMBER * _ROUNDS; i++) {
 			sum_time += at.get(i);
 		}
-		System.out.println(  "Latency: "+ sum_time/(_CLIENT_NUMBER*(_ROUNDS))+"ms");
-		System.out.print("TOTAL EXECUTION TIME: "+(System.currentTimeMillis()-startTime)+"ms");
+		System.out.println("Latency: " + sum_time / (_CLIENT_NUMBER * (_ROUNDS)) + "ms");
+		System.out.print("TOTAL EXECUTION TIME: " + (System.currentTimeMillis() - startTime) + "ms");
 		cache.destroy();
 		stale_cache.destroy();
-    System.out.print("\n======================================\n\n\n\n");
-    }
+		System.out.print("\n======================================\n\n\n\n");
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
