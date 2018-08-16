b8732891fd068aa906a3be614c48314d4416c6de
Kiarahmani <rahmank@purdue.edu>
2018-08-16 16:47:43 -0400

    1

--- src/main/java/com/mycompany/app/Constants.java
+++ src/main/java/com/mycompany/app/Constants.java
@@ -33,6 +33,7 @@ public class Constants {
 		for (int i = 0; i < _OBJECT_NUMBER; i++)
 			all_keys.add(i);
 		// create all district keys
+		all_keys_district = new TreeSet<DoubleKey>();
 		for (int w = 0; w < _WAREHOUSE_NUMBER; w++) {
 			for (int d = 0; d < _DISTRICT_NUMBER; d++)
 				all_keys_district.add(new DoubleKey(d, w));
