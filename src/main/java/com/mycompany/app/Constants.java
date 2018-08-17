e3eed4183d31fd281de99346c6194e66f51bc365
Kiarahmani <rahmank@purdue.edu>
2018-08-16 21:52:31 -0400

    1

--- src/main/java/com/mycompany/app/Constants.java
+++ src/main/java/com/mycompany/app/Constants.java
@@ -8,12 +8,10 @@ import org.apache.ignite.transactions.TransactionIsolation;
 
 public class Constants {
 	public int _CLIENT_NUMBER;
-	public int _OBJECT_NUMBER;
 	public int _DISTRICT_NUMBER;
 	public int _WAREHOUSE_NUMBER;
 	public int _TOTAL_REPS;
 	public int _ROUNDS;
-	Set<Integer> all_keys;
 	Set<DoubleKey> all_keys_district;
 	Set<Integer> all_keys_warehouse;
 
@@ -21,18 +19,15 @@ public class Constants {
 	TransactionIsolation rc;
 	TransactionConcurrency concurrency;
 
-	public Constants(int clientNumber, int objectNumber, int totals) {
+	public Constants(int clientNumber, int totals) {
 		// change these later //TODO
 		this._DISTRICT_NUMBER = 2;
 		this._WAREHOUSE_NUMBER = 3;
 
 		this._CLIENT_NUMBER = clientNumber;
-		this._OBJECT_NUMBER = objectNumber;
 		this._TOTAL_REPS = totals;
-		this._ROUNDS = _TOTAL_REPS / _CLIENT_NUMBER;
-		all_keys = new TreeSet<Integer>();
-		for (int i = 0; i < _OBJECT_NUMBER; i++)
-			all_keys.add(i);
+		this._ROUNDS = totals;// _TOTAL_REPS / _CLIENT_NUMBER;
+
 		// create all district keys
 		all_keys_district = new TreeSet<DoubleKey>();
 		for (int d = 0; d < _DISTRICT_NUMBER; d++) {
