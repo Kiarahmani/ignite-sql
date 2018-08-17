effd7f633865011b9f899181acfa965fed284f55
Kiarahmani <rahmank@purdue.edu>
2018-08-16 21:40:57 -0400

    1

--- src/main/java/com/mycompany/app/Client.java
+++ src/main/java/com/mycompany/app/Client.java
@@ -27,9 +27,10 @@ public class Client {
 		// randomly pick a district and update its (and its warehouse's) ytd
 		int w_id = ThreadLocalRandom.current().nextInt(0, cons._WAREHOUSE_NUMBER);
 		int d_id = ThreadLocalRandom.current().nextInt(0, cons._DISTRICT_NUMBER);
-		Warehouse wh = warehouse_cache.get(w_id);
+		
 		try (Transaction tx = transactions.txStart(cons.concurrency, cons.ser)) {
 			// update w_ytd
+			Warehouse wh = warehouse_cache.get(w_id);
 			warehouse_cache.put(w_id, new Warehouse(wh.w_name, wh.w_address, wh.w_tax, wh.w_ytd + 1, true));
 			tx.commit();
 			tx.close();
