59807717d052fcba93140c7477b0c733e867959c
Kiarahmani <rahmank@purdue.edu>
2018-08-18 18:54:18 -0400

    1

--- src/main/java/com/mycompany/app/Client.java
+++ src/main/java/com/mycompany/app/Client.java
@@ -101,14 +101,14 @@ public class Client {
 		Set<Integer> item_keys = new TreeSet<Integer>();
 		for (int i = 0; i < item_count; i++)
 			item_keys.add(ThreadLocalRandom.current().nextInt(0, cons._ITEM_NUMBER));
-
+		int w_tax = caches.warehouse_scache.get(wid).w_tax;
 		IgniteTransactions transactions = ignite.transactions();
 		try (Transaction tx = transactions.txStart(cons.concurrency, cons.ser)) {
 
 			DoubleKey d_key = new DoubleKey(did, wid);
 			TrippleKey c_key = new TrippleKey(cid, did, wid);
 			// read district and warehouse tax rate
-			int w_tax = caches.warehouse_cache.get(wid).w_tax;
+			
 			District dist = caches.district_cache.get(d_key);
 			/*int d_tax = dist.d_tax;
 			// update district's next order id
