43a884898d2057557754914971cbc3f2394c6cab
Kiarahmani <rahmank@purdue.edu>
2018-08-18 16:40:58 -0400

    1

--- src/main/java/com/mycompany/app/Client.java
+++ src/main/java/com/mycompany/app/Client.java
@@ -91,9 +91,10 @@ public class Client {
 	public long newOrder(Ignite ignite, Constants cons) {
 		long startTime = System.currentTimeMillis();
 		IgniteTransactions transactions = ignite.transactions();
+		try (Transaction tx = transactions.txStart(cons.concurrency, cons.ser)) {
 		IgniteCache<Integer, Warehouse> warehouse_cache = ignite.cache("warehouse_ser");
 		IgniteCache<DoubleKey, District> district_cache = ignite.cache("district_ser");
-		IgniteCache<TrippleKey, Customer> customer_cache = ignite.cache("customer_ser");
+		IgniteCache<TrippleKey, Customer> customer_cache = ignite.cache("customer_ser");}
 		
 		//IgniteCache<QuadKey, Order> order_cache = ignite.cache("order_ser");
 		//IgniteCache<Integer, Item> item_cache = ignite.cache("item_ser");
