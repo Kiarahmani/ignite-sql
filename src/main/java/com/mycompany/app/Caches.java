d8e50eef9db8a7218cd6023d22548145c5000014
Kiarahmani <rahmank@purdue.edu>
2018-08-19 16:30:24 -0400

    1

--- src/main/java/com/mycompany/app/Caches.java
+++ src/main/java/com/mycompany/app/Caches.java
@@ -35,31 +35,39 @@ public class Caches {
 	public Caches(Ignite ignite) {
 		NearCacheConfiguration<Integer, Warehouse> warehouse_nearCfg = new NearCacheConfiguration<Integer, Warehouse>();
 		this.warehouse_cache = ignite.cache("warehouse_ser");
-		this.warehouse_scache = ignite.getOrCreateNearCache("warehouse_stale",warehouse_nearCfg);
+		this.warehouse_scache = ignite.getOrCreateNearCache("warehouse_stale", warehouse_nearCfg);
 
+		NearCacheConfiguration<DoubleKey, District> district_nearCfg = new NearCacheConfiguration<DoubleKey, District>();
 		this.district_cache = ignite.cache("district_ser");
-		this.district_scache = ignite.cache("district_stale");
+		this.district_scache = ignite.getOrCreateNearCache("district_stale", district_nearCfg);
 
+		NearCacheConfiguration<TrippleKey, Customer> customer_nearCfg = new NearCacheConfiguration<TrippleKey, Customer>();
 		this.customer_cache = ignite.cache("customer_ser");
-		this.customer_scache = ignite.cache("customer_stale");
+		this.customer_scache = ignite.getOrCreateNearCache("customer_stale", customer_nearCfg);
 
+		NearCacheConfiguration<QuadKey, Order> order_nearCfg = new NearCacheConfiguration<QuadKey, Order>();
 		this.order_cache = ignite.cache("order_ser");
-		this.order_scache = ignite.cache("order_stale");
+		this.order_scache = ignite.getOrCreateNearCache("order_stale", order_nearCfg);
 
+		NearCacheConfiguration<Integer, Item> item_nearCfg = new NearCacheConfiguration<Integer, Item>();
 		this.item_cache = ignite.cache("item_ser");
-		this.item_scache = ignite.cache("item_stale");
+		this.item_scache = ignite.getOrCreateNearCache("item_stale", item_nearCfg);
 
+		NearCacheConfiguration<DoubleKey, Stock> stock_nearCfg = new NearCacheConfiguration<DoubleKey, Stock>();
 		this.stock_cache = ignite.cache("stock_ser");
-		this.stock_scache = ignite.cache("stock_stale");
+		this.stock_scache = ignite.getOrCreateNearCache("stock_stale", stock_nearCfg);
 
+		NearCacheConfiguration<TrippleKey, Boolean> newOrder_nearCfg = new NearCacheConfiguration<TrippleKey, Boolean>();
 		this.newOrder_cache = ignite.cache("newOrder_ser");
-		this.newOrder_scache = ignite.cache("newOrder_stale");
+		this.newOrder_scache = ignite.getOrCreateNearCache("newOrder_stale", newOrder_nearCfg);
 
+		NearCacheConfiguration<Integer, History> history_nearCfg = new NearCacheConfiguration<Integer, History>();
 		this.history_cache = ignite.cache("history_ser");
-		this.history_scache = ignite.cache("history_stale");
+		this.history_scache = ignite.getOrCreateNearCache("history_stale", history_nearCfg);
 
+		NearCacheConfiguration<TrippleKey, OrderLine> orderLine_nearCfg = new NearCacheConfiguration<TrippleKey, OrderLine>();
 		this.orderLine_cache = ignite.cache("orderLine_ser");
-		this.orderLine_scache = ignite.cache("orderLine_stale");
+		this.orderLine_scache = ignite.getOrCreateNearCache("orderLine_stale", orderLine_nearCfg);
 
 	}
 
