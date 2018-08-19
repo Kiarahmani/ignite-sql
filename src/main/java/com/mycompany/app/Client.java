a899cc299197c633d95e142928f2757c3d97baf2
Kiarahmani <rahmank@purdue.edu>
2018-08-18 20:20:23 -0400

    1

--- src/main/java/com/mycompany/app/Client.java
+++ src/main/java/com/mycompany/app/Client.java
@@ -130,6 +130,7 @@ public class Client {
 			caches.newOrder_cache.put(newOrder_key, true);
 			Map<Integer, Item> all_items = caches.item_cache.getAll(item_keys);
 			Map<DoubleKey, Stock> all_stocks = caches.stock_cache.getAll(stock_keys);
+			System.err.println(all_items.size());
 			for (DoubleKey st_key : stock_keys) {
 				// read the corresponding stock
 				int ol_quant = ThreadLocalRandom.current().nextInt(1, 11);
