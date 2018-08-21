58f7c3b7827983bb2068e00c6d1bf27aa54e1035
Kiarahmani <rahmank@purdue.edu>
2018-08-21 11:17:57 -0400

    1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -40,7 +40,7 @@ public class App {
 		boolean _COORDINATOR = Boolean.valueOf(args[0]);
 		int _FOLLOWER_COUNT = Integer.valueOf(args[1]);
 		Constants cons = new Constants(/* clients */ 2, /* totals */ 64, /* size */ 3, true);
-		Ignite ignite = new Starter("18.222.125.148", "18.222.69.139", /* server */"13.250.104.28").start();
+		Ignite ignite = new Starter("18.222.125.148", "18.222.69.139", /* server */"34.201.210.70").start();
 
 		if (_COORDINATOR) {
 			CacheManager manager = new CacheManager(ignite);
