e6eceb09f31b685a49edb6cf4a03f456fd08d770
Kiarahmani <rahmank@purdue.edu>
2018-08-22 14:42:53 -0400

    1

--- auto.sh
+++ auto.sh
@@ -1,5 +1,5 @@
 rm results.txt
 touch results.txt
 for i in 2 4 8 16 32 64 128;  
-do mvn package && mvn exec:java -Dexec.mainClass="com.mycompany.app.App" -Dexec.args="false 1 $i"; 
+do mvn package && mvn exec:java -Dexec.mainClass="com.mycompany.app.App" -Dexec.args="false 2 $i"; 
 done
--- auto_coordinate.sh
+++ auto_coordinate.sh
@@ -1,4 +1,4 @@
 for i in 2 4 8 16 32 64 128; 
-do mvn package && mvn exec:java -Dexec.mainClass="com.mycompany.app.App" -Dexec.args="true 1 $i"; 
+do mvn package && mvn exec:java -Dexec.mainClass="com.mycompany.app.App" -Dexec.args="true 2 $i"; 
 sleep 5;
 done
